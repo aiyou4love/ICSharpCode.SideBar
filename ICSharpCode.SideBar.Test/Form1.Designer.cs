2a186e1ec8d6acfc9c35edfaf7c40d567d3c75ad
zyouhua <aiyou4love@outlook.com>
2020-10-30 18:10:44 +0800

    u

---
+++ ICSharpCode.SideBar.Test/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace ICSharpCode.SideBar.Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ICSharpCode.SideBar.DefaultSideTabFactory defaultSideTabFactory1 = new ICSharpCode.SideBar.DefaultSideTabFactory();
+            ICSharpCode.SideBar.DefaultSideTabItemFactory defaultSideTabItemFactory1 = new ICSharpCode.SideBar.DefaultSideTabItemFactory();
+            this.sideBarControl1 = new ICSharpCode.SideBar.SideBarControl();
+            this.SuspendLayout();
+            // 
+            // sideBarControl1
+            // 
+            this.sideBarControl1.ActiveTab = null;
+            this.sideBarControl1.AllowDrop = true;
+            this.sideBarControl1.BackColor = System.Drawing.SystemColors.Control;
+            this.sideBarControl1.DoAddTab = false;
+            this.sideBarControl1.Location = new System.Drawing.Point(263, 61);
+            this.sideBarControl1.Name = "sideBarControl1";
+            this.sideBarControl1.SideTabFactory = defaultSideTabFactory1;
+            this.sideBarControl1.SideTabItemFactory = defaultSideTabItemFactory1;
+            this.sideBarControl1.Size = new System.Drawing.Size(406, 268);
+            this.sideBarControl1.TabIndex = 0;
+            this.sideBarControl1.Load += new System.EventHandler(this.sideBarControl1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sideBarControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SideBarControl sideBarControl1;
+    }
+}
+
---
+++ ICSharpCode.SideBar.Test/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICSharpCode.SideBar.Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sideBarControl1_Load(object sender, EventArgs e)
+        {
+            SideTab tab = sideBarControl1.Tabs.Add("name1");
+            tab.DisplayName = "text11";
+            tab.CanSaved = false;
+            tab.Items.Add("itemName11", "itemText11");
+            tab.Items.Add("itemName12", "itemText12");
+            tab.Items.Add("itemName13", "itemText13");
+            tab.CanBeDeleted = tab.CanDragDrop = false;
+
+
+            SideTab tab1 = sideBarControl1.Tabs.Add("name2");
+            tab1.DisplayName = "text21";
+            tab1.CanSaved = false;
+            tab1.Items.Add("itemName21", "itemText21");
+            tab1.Items.Add("itemName22", "itemText22");
+            tab1.Items.Add("itemName23", "itemText23");
+            tab1.CanBeDeleted = tab.CanDragDrop = false;
+            sideBarControl1.ActiveTab = tab1;
+
+            SideTab tab2 = sideBarControl1.Tabs.Add("name3");
+            tab2.DisplayName = "text31";
+            tab2.CanSaved = false;
+            tab2.Items.Add("itemName31", "itemText31");
+            tab2.Items.Add("itemName32", "itemText32");
+            tab2.Items.Add("itemName33", "itemText33");
+            tab2.CanBeDeleted = tab.CanDragDrop = false;
+        }
+    }
+}
